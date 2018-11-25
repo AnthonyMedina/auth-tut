01553d95456c0c722ff7053cbc034259fdfd4bb7
Anthony Medina <antmedina91@gmail.com>
2018-11-25 23:28:30 +0000

    add login route

--- server/app.js
+++ server/app.js
@@ -23,4 +23,17 @@ app.get('/', (req, res) => {
   res.json({ msg: `👋` });
 });
 
+app
+  .route('/login')
+  .get((req, res) => {
+    console.log('Inside GET /login callback function');
+    console.log(req.sessionID);
+    res.send(`You got the login page!\n`);
+  })
+  .post((req, res) => {
+    console.log('Inside POST /login callback function');
+    console.log(req.body);
+    res.send(`You posted to the login page!\n`);
+  });
+
 module.exports = app;
